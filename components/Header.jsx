edcdd6cebf54db7e926f8b732d6f0dddc757425a
hiren14 <hiren14lalni@gmail.com>
2023-09-27 08:00:38 +0530

    fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
     <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" className="flex items-center">
-                <img src="https://ewastefinder.vercel.app/ewaste-logo.jpg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                <img src="/ewaste-logo.jpg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Ewaste Finder</span>
             </a>
             <div className="flex items-center lg:order-2">
